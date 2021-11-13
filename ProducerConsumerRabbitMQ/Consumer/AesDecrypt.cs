05b8d3214aa8f9c9190447088ffcaacff0745bf7
ron leib <57533765+ronleib@users.noreply.github.com>
2021-11-13 23:14:15 +0200

    Add files via upload

---
+++ ProducerConsumerRabbitMQ/Consumer/AesDecrypt.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Consumer
+{
+    public class AesDecrypt
+    {
+        public static string EncryptionKey = "W),^#&*LK5xv&<(D(9";
+
+        public static string Decrypt(string cipherText)
+        {
+            cipherText = cipherText.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+    }
+}
---
+++ ProducerConsumerRabbitMQ/Consumer/MainConsumer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    public class Class_Main_Consumer
+    {
+        public static void Main(string[] args)
+        {
+        
+              Receiver.Rec();
+            
+            //Encryption_Aes_Decrypt.Aes_Decrypt(ReceiverTemp);
+            //Console.WriteLine("Received message {0} ....", ReceiverTemp);
+
+        }
+    }
+}
